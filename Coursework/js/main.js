086e0241bf8f7972c95a12dbc555377128055a96
Haaruuns <haaruun.sugulle@student.lut.fi>
2025-10-28 14:35:10 +0200

    Added JavaScript toggle logic

---
+++ Coursework/js/main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.querySelector(".faq-content")
+    //console.log(content)
+
+    content.addEventListener("click", (e) => {
+        const groupHeader = e.target.closest(".faq-group-header")
+        if (!groupHeader) {return}
+        // console.log(groupHeader)
+
+        const group =  groupHeader.parentElement
+        const body = group.querySelector(".faq-group-body")
+        const icon = group.querySelector("i")
+        //console.log(group)
+        //console.log(body)
+        //console.log(icon)        
+
+        icon.classList.toggle("fa-minus")
+        icon.classList.toggle("fa-plus")
+
+        body.classList.toggle("open")
+
+        // Close other
+        const allGroups = content.querySelectorAll(".faq-group")
+        //console.log(allGroups)
+
+        allGroups.forEach((a_group) => {
+            if (a_group !==group) {
+                const a_groupBody = a_group.querySelector(".faq-group-body")
+                const a_groupIcon = a_group.querySelector("i")
+
+                a_groupBody.classList.remove("open")
+                a_groupIcon.classList.remove("fa-minus")
+                a_groupIcon.classList.add("fa-plus")
+            }
+        })
+
+
+    })
+})
